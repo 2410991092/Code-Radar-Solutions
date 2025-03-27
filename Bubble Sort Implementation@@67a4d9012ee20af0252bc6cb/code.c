f72d741a4a174bb2221012fb1fe340b3cbcc75a8
2410991092 <dolygarg592@gmail.com>
2025-03-27 23:15:24 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main(){
+    void Bubble();
+    Bubble();
+}
+void Bubble(){
     int n;
     scanf("%d",&n);
     int A[n];
     for(int r=0;r<n;r++){
         scanf("%d",&A[r]);
     }
-    
+
 }
